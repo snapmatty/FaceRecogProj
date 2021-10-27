c7f74dbb7e42d84b2b63aa6ee1a9b405bc75a038
snapmatty <matdrygiel@gmail.com>
2021-10-27 15:10:27 +0200

    added gauss noise v2
    gauss noise should be applied before the rotation, the idea is as follows:
    
    1.append images to list
    2. change list into array
    3. in gauss function provide the array as input for noise application
    4.input this function as pre-processin in ImageDataGenerator
    5. generate data as batch into folder "testimages"
    
    Had to re-list the previous push because i forgot to delete the created batch (it was over 5000 images)

--- data_aug.py
+++ data_aug.py
@@ -1,29 +1,46 @@
 import cv2
-from keras_preprocessing.image import ImageDataGenerator, io
+from keras_preprocessing.image import ImageDataGenerator
 import numpy as np
-from matplotlib import pyplot
-from PIL import Image
-from skimage import io
-from skimage.filters import gaussian
+import glob
+import random
+
+# images = dir('dataset/')
+# noise_gauss2 = np.random.normal(0,1,100)
+# noise_gauss = random_noise(images, mode='gaussian', seed = None, clip = True)
+
+
+cv_img = []
+for img in glob.glob("dataset/*.jpg"):
+    n = cv2.imread(img)
+    cv_img.append(n)
+
+array_of_img = np.array(cv_img)
+
+def add_gauss_noise(array_of_img):
+    '''Add random noise to an image'''
+    var = 50
+    dev = var * random.random()
+    noise = np.random.normal(0, dev, array_of_img.shape)
+    array_of_img += noise
+    np.clip(array_of_img, 0., 255.)
+    return array_of_img
 
 
 generating_data = ImageDataGenerator(
-        rotation_range = 5,
-        horizontal_flip = True,
+    rotation_range=5,
+    preprocessing_function = add_gauss_noise(array_of_img)
 )
 
 i = 0
 for batch in generating_data.flow_from_directory(
-                                directory='dataset/',
-                                color_mode='rgb',
-                                batch_size = 2,
-                                interpolation= "bilinear",
-                                shuffle = True,
-                                target_size= (256,256),
-                                save_to_dir = 'testofimages',
-                                save_prefix = 'aug',
-                                save_format = 'jpg'):
-    i +=1
-    if i>20:
+        directory='dataset/',
+        color_mode='rgb',
+        batch_size=50,
+        shuffle=True,
+        target_size=(256, 256),
+        save_to_dir='testofimages',
+        save_prefix='aug',
+        save_format='jpg'):
+    i += 1
+    if i > 300:
         break
-
