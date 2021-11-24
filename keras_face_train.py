8549c09c7c131dc873fc6661f9daa97aef6aab2b
snapmatty <matdrygiel@gmail.com>
2021-11-24 23:05:07 +0100

    Commit
    - commit not working when a lot of image data in project, hence all img were removed for code commit

--- keras_face_train.py
+++ keras_face_train.py
@@ -9,43 +9,43 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 train = ImageDataGenerator()
 valid = ImageDataGenerator()
-train_dataset = train.flow_from_directory('dataset/aug/train',
+traindataset = train.flow_from_directory('dataset/aug/train',
                                           target_size=(64, 64),
                                           batch_size=4,
                                           class_mode='binary')
 
-valid_dataset = valid.flow_from_directory('dataset/aug/valid',
+validdataset = valid.flow_from_directory('dataset/aug/valid',
                                           target_size=(64, 64),
                                           batch_size=4,
                                           class_mode='binary')
 
 model = tf.keras.models.Sequential([tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(64, 64, 1)),
-                                    tf.keras.layers.MaxPool2D(2, 2),
+                                    tf.keras.layers.MaxPool2D(2, 2),BatchNormalization(2),
+
                                     #
                                     tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
                                     tf.keras.layers.MaxPool2D(2, 2),
+                                    Dropout(0.2),
                                     #
                                     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
                                     tf.keras.layers.MaxPool2D(2, 2),
+                                    BatchNormalization(2),
                                     #
                                     tf.keras.layers.Flatten(),
                                     #
-                                    tf.keras.layers.Dense(512, activation = 'relu'),
-                                    #
-                                    tf.keras.layers.Dense(1, activation = 'softmax')
+                                    tf.keras.layers.Dense(1, activation = 'sigmoid')
                                     ])
-model.add(Dropout(0.2))
-model.add(BatchNormalization(2))
+
+
 
 model.summary()
 
 model.compile(loss= 'binary_crossentropy',
-              optimizer=adam_v2(learning_rate=0.001),
               metrics=['accuracy'])
 
-model_fit = model.fit(training_data = train_dataset,
-                      validation_data=valid_dataset,
-                      step_per_epoch = 3,
+model_fit = model.fit(traindataset,
+                     validdataset,
+                      batch_size = 32,
                       epochs = 20,
                       verbose =2
-                    )
+                   )
